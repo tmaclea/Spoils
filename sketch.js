29f12c0b77b4f4f16fe56e84b9d40324138a740c
tmaclea <macleanis@hotmail.com>
2017-07-23 12:27:55 -0700

    fixed zombie looping bug that caused a fatal error

--- sketch.js
+++ sketch.js
@@ -48,16 +48,18 @@ function draw() {
         if(zombies[i].isInRange(player)) {
             zombies[i].attack(player);
         }
-        if(zombies[i].dead()){
-            player.killCount++;
-            parts = parts.concat(zombies[i].die());
-            zombies.splice(i, 1);
-        }
+        
         //reduce zombie overlap
         for(var j = zombies.length - 1; j >= 0; j--) {
             if(zombies[i].isInRange(zombies[j])) {
                 zombies[i].noOverlap(zombies[j]);
             }
+        }
+
+        if(zombies[i].dead()){
+            player.killCount++;
+            parts = parts.concat(zombies[i].die());
+            zombies.splice(i, 1);
         }
     }
     //make sure there are always 200 zombies
